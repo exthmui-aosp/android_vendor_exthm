caf74a7cfc33c3c6b78c4256236795ea78216af8
XayahSuSuSu <zds1249475336@gmail.com>
2022-07-30 14:08:42 +0800

    build: Add modular build hooks
    * Ref: https://github.com/ProtonAOSP/android_vendor_proton

---
+++ build/init.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 The exTHmUI Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# exTHmUI version
+-include vendor/exthm/build/tools/version.mk
---
+++ build/tools/version.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The exTHmUI Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+EXTHM_VERSION := 13.0
+
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.build.version.custom=$(EXTHM_VERSION)
