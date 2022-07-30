68e0e41ea7e31e46c4e758e7bda165ed4da7cdc5
TH779 <i@779.moe>
2022-07-30 20:05:44 +0800

    vendor: prebuilt: Checkout apns-conf.xml from LineageOS * From LineageOS/android_vendor_lineage@2007edd.

--- config/phone.mk
+++ config/phone.mk
@@ -7,6 +7,10 @@
 # Inherit common exTHmUI stuff
 $(call inherit-product, vendor/exthm/config/common.mk)
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/exthm/prebuilt/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+
 # Telephony packages
 PRODUCT_PACKAGES += \
     messaging \
