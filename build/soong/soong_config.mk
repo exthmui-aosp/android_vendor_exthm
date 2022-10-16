YuyukoAOSPMod/vendor_aosp

build: soong: _contents -> _json_contents

build/soong/soong_config.mk
@@ -1,8 +1,8 @@
-_contents := $(_contents)    "Aosp":{$(newline)
+_json_contents := $(_json_contents)    "Aosp":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
+_json_contents := $(_json_contents)__SV_END
 
-_contents := $(_contents)    },$(newline)
+_json_contents := $(_json_contents)    },$(newline)

exthmui-aosp/android_vendor_exthm

build/soong/soong_config.mk
@@ -1,8 +1,8 @@
-_contents := $(_contents)    "Exthm":{$(newline)
+_json_contents := $(_json_contents)    "Exthm":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
+_json_contents := $(_json_contents)__SV_END
 
-_contents := $(_contents)    },$(newline)
+_json_contents := $(_json_contents)    },$(newline)