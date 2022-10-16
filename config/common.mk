YuyukoAOSPMod/vendor_aosp

config: Set default sounds

config/common.mk
@@ -11,6 +11,9 @@ $(call inherit-product, vendor/aosp/overlay/overlay.mk)
 # YuyukoAOSPMod version
 -include vendor/aosp/config/version.mk
 
+# Sounds
+include vendor/aosp/config/sounds.mk
+
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME \

exthmui-aosp/android_vendor_exthm

config/common.mk
@@ -10,6 +10,9 @@ $(call inherit-product, vendor/exthm/overlay/overlay.mk)
 # exTHmUI version
 -include vendor/exthm/config/version.mk
 
+# Sounds
+include vendor/exthm/config/sounds.mk
+
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME \