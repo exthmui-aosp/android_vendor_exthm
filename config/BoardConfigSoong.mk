YuyukoAOSPMod/vendor_aosp

build: soong: Add aosp soong config variables Ref: https://android.googlesource.com/platform/build/soong/+/refs/tags/android-11.0.0_r4#soong-config-variables

config/BoardConfigSoong.mk
@@ -25,3 +25,44 @@ define addVar
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+
+SOONG_CONFIG_NAMESPACES += aospGlobalVars
+SOONG_CONFIG_aospGlobalVars += \
+    additional_gralloc_10_usage_bits \
+    target_init_vendor_lib \
+    target_ld_shim_libs \
+    target_process_sdk_version_override \
+    target_surfaceflinger_fod_lib
+
+SOONG_CONFIG_NAMESPACES += aospQcomVars
+SOONG_CONFIG_aospQcomVars += \
+    supports_extended_compress_format \
+    supports_hw_fde \
+    supports_hw_fde_perf \
+    uses_pre_uplink_features_netmgrd
+
+# Only create soong_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
+ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+SOONG_CONFIG_aospQcomVars += \
+    qcom_soong_namespace
+endif
+
+# Soong bool variables
+SOONG_CONFIG_aospQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
+SOONG_CONFIG_aospQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
+SOONG_CONFIG_aospQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_PERF)
+SOONG_CONFIG_aospQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+
+# Set default values
+TARGET_INIT_VENDOR_LIB ?= vendor_init
+TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
+
+# Soong value variables
+SOONG_CONFIG_aospGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
+SOONG_CONFIG_aospGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+SOONG_CONFIG_aospGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
+SOONG_CONFIG_aospGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
+SOONG_CONFIG_aospGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
+ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+SOONG_CONFIG_aospQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
+endif

exthmui-aosp/android_vendor_exthm

config/BoardConfigSoong.mk
@@ -25,3 +25,44 @@ define addVar
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+
+SOONG_CONFIG_NAMESPACES += exthmGlobalVars
+SOONG_CONFIG_exthmGlobalVars += \
+    additional_gralloc_10_usage_bits \
+    target_init_vendor_lib \
+    target_ld_shim_libs \
+    target_process_sdk_version_override \
+    target_surfaceflinger_fod_lib
+
+SOONG_CONFIG_NAMESPACES += exthmQcomVars
+SOONG_CONFIG_exthmQcomVars += \
+    supports_extended_compress_format \
+    supports_hw_fde \
+    supports_hw_fde_perf \
+    uses_pre_uplink_features_netmgrd
+
+# Only create soong_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
+ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+SOONG_CONFIG_exthmQcomVars += \
+    qcom_soong_namespace
+endif
+
+# Soong bool variables
+SOONG_CONFIG_exthmQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
+SOONG_CONFIG_exthmQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
+SOONG_CONFIG_exthmQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_PERF)
+SOONG_CONFIG_exthmQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+
+# Set default values
+TARGET_INIT_VENDOR_LIB ?= vendor_init
+TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
+
+# Soong value variables
+SOONG_CONFIG_exthmGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
+SOONG_CONFIG_exthmGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+SOONG_CONFIG_exthmGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
+SOONG_CONFIG_exthmGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
+SOONG_CONFIG_exthmGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
+ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+SOONG_CONFIG_exthmQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
+endif