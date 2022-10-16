YuyukoAOSPMod/vendor_aosp

overlay: Add product overlay
* We're going to move most of our config changes here for portability.

Co-authored-by: XayahSuSuSu <zds1249475336@gmail.com>

build/init.mk
@@ -5,5 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit vendor submodules
+$(call inherit-product, vendor/acme/overlay/overlay.mk)
+
 # YuyukoAOSPMod version
 -include vendor/aosp/build/tools/version.mk

exthmui-aosp/android_vendor_exthm

build/init.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit vendor submodules
+$(call inherit-product, vendor/exthm/overlay/overlay.mk)
+
 # exTHmUI version
 -include vendor/exthm/build/tools/version.mk