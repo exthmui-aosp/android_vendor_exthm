YuyukoAOSPMod/vendor_aosp

config: Move perl base modules to common prebuilts dir

config/BoardConfigKernel.mk
@@ -131,7 +131,7 @@ endif
 TOOLS_PATH_OVERRIDE := \
     PATH=$(BUILD_TOP)/prebuilts/tools-aosp/$(HOST_OS)-x86/bin:$$PATH \
     LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/tools-aosp/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
-    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-aosp/$(HOST_OS)-x86/lib/perl-base
+    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-aosp/common/lib/perl-base
 
 # Set DTBO image locations so the build system knows to build them
 ifeq ($(TARGET_NEEDS_DTBOIMAGE),true)

exthmui-aosp/android_vendor_exthm

config/BoardConfigKernel.mk
@@ -131,7 +131,7 @@ endif
 TOOLS_PATH_OVERRIDE := \
     PATH=$(BUILD_TOP)/prebuilts/tools-exthm/$(HOST_OS)-x86/bin:$$PATH \
     LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/tools-exthm/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
-    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-exthm/$(HOST_OS)-x86/lib/perl-base
+    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-exthm/common/lib/perl-base
 
 # Set DTBO image locations so the build system knows to build them
 ifeq ($(TARGET_NEEDS_DTBOIMAGE),true)