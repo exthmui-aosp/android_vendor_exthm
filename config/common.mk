YuyukoAOSPMod/vendor_aosp

vendor: Move makefile to config

config/common.mk
@@ -9,4 +9,4 @@
 $(call inherit-product, vendor/acme/overlay/overlay.mk)
 
 # YuyukoAOSPMod version
--include vendor/aosp/build/tools/version.mk
+-include vendor/aosp/config/version.mk

exthmui-aosp/android_vendor_exthm

config/common.mk
@@ -8,4 +8,4 @@
 $(call inherit-product, vendor/exthm/overlay/overlay.mk)
 
 # exTHmUI version
--include vendor/exthm/build/tools/version.mk
+-include vendor/exthm/config/version.mk