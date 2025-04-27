2ccf68653189530a67d4276c00c66114d1e4b23f
chandu078 <chandudyavanapelli03@gmail.com>
2025-04-27 14:38:48 +0530

    treewide: Clean some LineageOS stuff
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- config/telephony.mk
+++ config/telephony.mk
@@ -1,24 +1,10 @@
-# GMS
-ifeq ($(WITH_GMS),true)
-WITH_GMS_COMMS_SUITE := true
-endif
-
-# Sensitive Phone Numbers list
-PRODUCT_PACKAGES += \
-    sensitive_pn.xml
-
 # World APN list
 PRODUCT_PACKAGES += \
     apns-conf.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \
-    messaging \
     Stk
-
-# Default ringtone
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=Orion.ogg
 
 # Tethering - allow without requiring a provisioning app
 # (for devices that check this)
